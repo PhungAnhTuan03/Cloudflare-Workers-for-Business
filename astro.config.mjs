05665c04150964bad2616fb017d72310de0657d6
PhungAnhTuan_9469 <phungtuan23122003@gmail.com>
2026-05-15 09:07:50 +0700

    update worker config

---
+++ astro.config.mjs
@@ -0,0 +1,33 @@
+import cloudflare from "@astrojs/cloudflare";
+import react from "@astrojs/react";
+import { d1, r2 } from "@emdash-cms/cloudflare";
+import { defineConfig } from "astro/config";
+import emdash from "emdash/astro";
+
+export default defineConfig({
+	output: "server",
+	adapter: cloudflare({
+		platformProxy: {
+			enabled: true
+		}
+	}),
+
+	image: {
+		layout: "constrained",
+		responsiveStyles: true,
+	},
+
+	integrations: [
+		react(),
+		emdash({
+			database: d1({ binding: "DB", session: "auto" }),
+			storage: r2({ binding: "MEDIA" }),
+		}),
+	],
+
+	devToolbar: { enabled: false },
+
+	//   vite: {
+	//     plugins: [tailwindcss()],
+	//   },
+});
