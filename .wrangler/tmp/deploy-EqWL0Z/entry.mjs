be75f86ffcdc47969da7365fc718789101f38906
PhungAnhTuan_9469 <phungtuan23122003@gmail.com>
2026-05-29 17:07:04 +0700

    fix deploy

--- .wrangler/tmp/deploy-EqWL0Z/entry.mjs
+++
@@ -1,8 +0,0 @@
-globalThis.process ??= {};
-globalThis.process.env ??= {};
-import { P, w } from "./chunks/worker-entry_B14YdcPd.mjs";
-import "cloudflare:workers";
-export {
-  P as PluginBridge,
-  w as default
-};
